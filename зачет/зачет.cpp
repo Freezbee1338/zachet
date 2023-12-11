95fe29c3d958f9947b164f8c4261ad6fa1316577
freez <freez@DESKTOP-C59PP5D>
2023-12-12 01:29:14 +0300

    Добавьте файлы проекта.

---
+++ зачет/зачет.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class CurrencyConverter {
+public:
+    double usdToEur(double usd) {
+        return usd * 0.92; // Курс доллара к евро
+    }
+
+    double eurToUsd(double eur) {
+        return eur * 1.08; // Курс евро к доллару
+    }
+};
+
+int main() {
+    CurrencyConverter converter;
+
+    setlocale(LC_ALL, "ru");
+
+    double amount;
+    string currency;
+
+    cout << "Введите сумму: ";
+    cin >> amount;
+
+    cout << "Введите валюту (USD,EUR): ";
+    cin >> currency;
+
+    if (currency == "USD") {
+        cout << amount << " Доллар равен " << converter.usdToEur(amount) << " EUR" << endl;
+    }
+    else if (currency == "EUR") {
+        cout << amount << " ЕВРО равен " << converter.eurToUsd(amount) << " USD" << endl;
+    }
+    else {
+        cout << "Неверный ввод валюты" << endl;
+    }
+
+    return 0;
+}
